7127be5ebc2094e26a26d06e606519822ed12906
dueduardoful <dueduardoful@gmail.com>
2020-03-26 19:14:59 -0300

    projeto criado

---
+++ src/main/java/com/qintess/hibernate/App.java
@@ -0,0 +1,35 @@
+package com.qintess.hibernate;
+
+
+
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.qintess.hibernate.model.Student;
+import com.qintess.hibernate.util.HibernateUtil;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Student student = new Student("Eduardo", "Batista", "dueduardoful@gmail.com");
+		Transaction transaction = null;
+		try(Session session = HibernateUtil.getSessionFactory().openSession()){
+			
+			
+			// start transaction
+			transaction = session.beginTransaction();
+			// salvar o objeto student
+			session.save(student);
+			// commit transaction
+			transaction.commit();
+		}
+		catch(Exception e) {
+			if(transaction != null) {
+					transaction.rollback();
+			}
+		}
+
+	}
+
+}
